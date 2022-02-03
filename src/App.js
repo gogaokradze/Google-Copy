5f3de78f0b89244f302711db13eb11c6456a3444
Goga Okradze <deathofsuper@gmail.com>
2022-02-03 17:27:45 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import RoutesComp from "./components/RoutesComp";
 
 const App=()=>{
   const [darkTheme, setDarkTheme] = useState(false);
-  return(
+   return(
     <div className={darkTheme ?'dark':''}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
          <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import Search from './Search';
     <div className="flex justify-between items-center space-x-5 w-screen ">
       <Link to="/">
         <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
-          Google 🔎
+          Google 
         </p>
       </Link>
       <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">{darkTheme ? '💡 Light' : '🌙 Dark'}</button>
